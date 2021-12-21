83deb03431db6f24cd75d61584e3faee691a6d47
Amin Boukari <ambouk3@gmail.com>
2021-12-20 21:58:32 -0500

    Remove if name main check

--- train.py
+++ train.py
@@ -14,112 +14,112 @@ from tqdm import trange
 
 from utils import WikiTextDataset, build_model, build_optimizer
 
-if __name__ == '__main__':
-    config = yaml.safe_load(open('config.yml', 'r'))
 
-    device = torch.device(config['device'])
-    data_dir = config['data']['data_dir']
+config = yaml.safe_load(open('config.yml', 'r'))
 
-    print("Loading Imagenet")
-    image_transforms = transforms.Compose([
-        transforms.Resize((config['model']['image_size'],
-                           config['model']['image_size'])),
-        transforms.ToTensor()
-    ])
+device = torch.device(config['device'])
+data_dir = config['data']['data_dir']
 
-    imagenet_dataset = imagesdatasets.ImageNet(
-        config['data']['imagenet_dir'], transform=image_transforms)
-    imagenet_loader = DataLoader(imagenet_dataset, shuffle=True,
-                                 batch_size=config['training']['image_batch_size'], pin_memory=True, drop_last=True)
+print("Loading Imagenet")
+image_transforms = transforms.Compose([
+    transforms.Resize((config['model']['image_size'],
+                       config['model']['image_size'])),
+    transforms.ToTensor()
+])
 
-    print("Loading WikiText103")
-    if not os.path.exists(os.path.join(data_dir, config['data']['wikitext_dataset'])):
-        tokenizer = Tokenizer(BPE.from_file(
-            vocab=os.path.join(data_dir, 'vocab.json'), merges=os.path.join(data_dir, 'merges.txt')))
-        text_dataset = WikiTextDataset(
-            config['data']['wikitext_dir'], split='train', tokenizer=tokenizer, seq_len=config['model']['seq_len'], num_preprocessing_workers=config['data']['num_preprocessing_workers'])
-        text_dataset.save(os.path.join(
-            data_dir, config['data']['wikitext_dataset']))
-    else:
-        text_dataset = WikiTextDataset.from_preprocessed(
-            os.path.join(data_dir, config['data']['wikitext_dataset']), seq_len=config['model']['seq_len'])
+imagenet_dataset = imagesdatasets.ImageNet(
+    config['data']['imagenet_dir'], transform=image_transforms)
+imagenet_loader = DataLoader(imagenet_dataset, shuffle=True,
+                             batch_size=config['training']['image_batch_size'], pin_memory=True, drop_last=True)
 
-    text_loader = DataLoader(
-        text_dataset, batch_size=config['training']['text_batch_size'], pin_memory=True, drop_last=True)
+print("Loading WikiText103")
+if not os.path.exists(os.path.join(data_dir, config['data']['wikitext_dataset'])):
+    tokenizer = Tokenizer(BPE.from_file(
+        vocab=os.path.join(data_dir, 'vocab.json'), merges=os.path.join(data_dir, 'merges.txt')))
+    text_dataset = WikiTextDataset(
+        config['data']['wikitext_dir'], split='train', tokenizer=tokenizer, seq_len=config['model']['seq_len'], num_preprocessing_workers=config['data']['num_preprocessing_workers'])
+    text_dataset.save(os.path.join(
+        data_dir, config['data']['wikitext_dataset']))
+else:
+    text_dataset = WikiTextDataset.from_preprocessed(
+        os.path.join(data_dir, config['data']['wikitext_dataset']), seq_len=config['model']['seq_len'])
 
-    imagenet_cycler = itertools.cycle(imagenet_loader)
-    text_cycler = itertools.cycle(text_loader)
+text_loader = DataLoader(
+    text_dataset, batch_size=config['training']['text_batch_size'], pin_memory=True, drop_last=True)
 
-    loader = itertools.chain.from_iterable(zip(imagenet_cycler, text_cycler))
+imagenet_cycler = itertools.cycle(imagenet_loader)
+text_cycler = itertools.cycle(text_loader)
 
-    print("Building Model & Optimizer")
-    image_transformer, text_transformer, num_parameters = build_model(
-        num_classes=1000, vocab_len=text_dataset.vocab_size, **config['model'])
+loader = itertools.chain.from_iterable(zip(imagenet_cycler, text_cycler))
 
-    image_transformer.to(device)
-    text_transformer.to(device)
+print("Building Model & Optimizer")
+image_transformer, text_transformer, num_parameters = build_model(
+    num_classes=1000, vocab_len=text_dataset.vocab_size, **config['model'])
 
-    print(f"Number of transformer parameters: {num_parameters:,}")
+image_transformer.to(device)
+text_transformer.to(device)
 
-    optimizer = build_optimizer(
-        image_transformer, text_transformer, config['optimizer'])
-    scaler = GradScaler()
-    loss_fn = nn.CrossEntropyLoss()
+print(f"Number of transformer parameters: {num_parameters:,}")
 
-    image_transformer.train()
-    text_transformer.train()
+optimizer = build_optimizer(
+    image_transformer, text_transformer, config['optimizer'])
+scaler = GradScaler()
+loss_fn = nn.CrossEntropyLoss()
 
-    running_loss_text = 0.0
-    running_loss_images = 0.0
+image_transformer.train()
+text_transformer.train()
 
-    image_steps = 0
-    text_steps = 0
+running_loss_text = 0.0
+running_loss_images = 0.0
 
-    mask = torch.triu(torch.ones(config['model']['seq_len'], config['model']
-                                 ['seq_len'], device=device) * float('-inf'), diagonal=1)
+image_steps = 0
+text_steps = 0
 
-    progressbar = trange(config['training']['train_iters'], desc='Training')
-    for step in progressbar:
-        using_images = step % 2 == 0
-        using_text = step % 2 == 1
-        if using_images:
-            model = image_transformer
-            image_steps += 1
+mask = torch.triu(torch.ones(config['model']['seq_len'], config['model']
+                             ['seq_len'], device=device) * float('-inf'), diagonal=1)
 
-        elif using_text:
-            model = text_transformer
-            text_steps += 1
+progressbar = trange(config['training']['train_iters'], desc='Training')
+for step in progressbar:
+    using_images = step % 2 == 0
+    using_text = step % 2 == 1
+    if using_images:
+        model = image_transformer
+        image_steps += 1
 
-        data = next(loader)
+    elif using_text:
+        model = text_transformer
+        text_steps += 1
 
-        src, tgt = data
-        src, tgt = src.to(device), tgt.to(device)
+    data = next(loader)
 
-        with autocast():
-            output = model(src)
+    src, tgt = data
+    src, tgt = src.to(device), tgt.to(device)
 
-            if using_text:
-                output = output.view(-1, output.size(-1))
-                tgt = tgt.view(-1)
+    with autocast():
+        output = model(src)
 
-            loss = loss_fn(output, tgt)
+        if using_text:
+            output = output.view(-1, output.size(-1))
+            tgt = tgt.view(-1)
 
-        if using_images:
-            running_loss_images += loss.item()
-        elif using_text:
-            running_loss_text += loss.item()
+        loss = loss_fn(output, tgt)
 
-        progressbar.set_description(
-            f'Training, Text Loss: {running_loss_text / (text_steps + 1):.3f}, Image Loss: {running_loss_images / (image_steps + 1):.3f}')
+    if using_images:
+        running_loss_images += loss.item()
+    elif using_text:
+        running_loss_text += loss.item()
 
-        scaler.scale(loss).backward()
+    progressbar.set_description(
+        f'Training, Text Loss: {running_loss_text / (text_steps + 1):.3f}, Image Loss: {running_loss_images / (image_steps + 1):.3f}')
 
-        if step % config['training']['gradient_accumulation_steps'] == 0:
-            nn.utils.clip_grad_norm_(
-                model.parameters(), config['training']['gradient_clipping'])
+    scaler.scale(loss).backward()
 
-            scaler.unscale_(optimizer)
-            scaler.step(optimizer)
-            scaler.update()
+    if step % config['training']['gradient_accumulation_steps'] == 0:
+        nn.utils.clip_grad_norm_(
+            model.parameters(), config['training']['gradient_clipping'])
 
-            optimizer.zero_grad(set_to_none=True)
+        scaler.unscale_(optimizer)
+        scaler.step(optimizer)
+        scaler.update()
+
+        optimizer.zero_grad(set_to_none=True)
